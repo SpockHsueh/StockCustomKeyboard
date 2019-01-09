cea1f60ae92f3c5987f02e7b605860c36a728590
金融研發一部-薛宇振 <spock@mitake.com.tw>
2019-01-09 15:11:55 +0800

    finish the stockKeyboard xib UI

--- StockCustomKeyboard/Views/StockKeyboardView.swift
+++ StockCustomKeyboard/Views/StockKeyboardView.swift
@@ -16,8 +16,72 @@ protocol StockKeyboardViewDelegate: AnyObject {
 }
 
 // Contains all of the logic for handling button taps and translating that info specifi actions on the text entry associated with it
-class StockKeyboardView: UIView {
+public class StockKeyboardView: UIView {
 
+    @IBOutlet weak var keyboardControlView: UIView!
+    @IBOutlet weak var nextKeyboardButton: UIButton!
+    private var allTextButtons = [UIButton]()
+    
+    weak var delegate: StockKeyboardViewDelegate?
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setNextKeyboardVisible(false)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    private func setNextKeyboardVisible(_ visible: Bool) {
+        nextKeyboardButton.isHidden = !visible
+    }
+    
+    private func setupButton() {
+        let nums = ["1","2","3","4","5","6","7","8","9",".","0","00"]
+        
+        let numsRow = addValuesOnKeyboard(kbKeys: nums)
+    }
+    
+    
+    // Adding keys on UiView with UiStack view
+    private func addValuesOnKeyboard(kbKeys: [String]) -> UIView {
+        
+        let rowStackView = UIStackView.init()
+        rowStackView.spacing = 5
+        rowStackView.axis = .horizontal
+        rowStackView.alignment = .fill
+        rowStackView.distribution = .fillEqually
+        
+        for key in kbKeys {
+            rowStackView.addArrangedSubview(createButtonWithTitle(title: key))
+        }
+        
+        let keysView = UIView()
+        keysView.backgroundColor = .red
+        keysView.addSubview(rowStackView)
+        keysView.
+        
+    }
+    
+    private func createButtonWithTitle(title: String) -> UIButton {
+        let button = UIButton(type: .system)
+        button.setTitle(title, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        allTextButtons.append(button)
+        return button
+    }
+    
+    @objc func didTapButton(_ sender: UIButton) {
+        
+    }
+    
+    
+}
 
-
+// MARK: - Actions
+extension StockKeyboardView {
+    
 }
